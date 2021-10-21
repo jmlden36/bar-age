47605420704524756f0b8fc0a74fbff11e8379d5
Matt Luker <jamesmattluker@gmail.com>
2021-10-21 13:46:01 -0600

    make whole site

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  const age = parseInt(prompt("How old are you?"));
+
+  if (age >= 21) {
+    $('#drinks').show();
+  } else if (age === 21) {
+    alert("Now don't go wild!");
+    $('#drinks').show();
+  } else {
+    $('#under-21').show();
+    alert("Go Home!");
+  }
+});
